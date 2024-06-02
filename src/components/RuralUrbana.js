06aa6b428c46aae080a900cc2c5adfe41f888927
Eriick1945 <162646349+Eriick1945@users.noreply.github.com>
2024-06-02 23:05:22 +0000

    Alterações_Erick

--- src/components/RuralUrbana.js
+++ src/components/RuralUrbana.js
@@ -1,68 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
+import Parse from 'parse/dist/parse.min.js';
+
+Parse.initialize(
+  'e5UX3sOvZfOY3qwvIdw0b7v18JdUn9ihz34chyhF', // Application ID
+  'rSRY7ybwkJThcFwIQirObv3v83Bfa40LWPyaZQNf' // JavaScript key
+);
+Parse.serverURL = 'https://parseapi.back4app.com';
 
 const UrbanRuralSchoolsChart = () => {
   const [chartData, setChartData] = useState({});
 
   useEffect(() => {
-    const urbanRuralData = [
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 2, 1, 1, 1, 1, 1,
-      1, 1, 2, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    ];
+    const fetchUrbanRuralData = async () => {
+      const Dashboard = Parse.Object.extend('Dashboard');
+      const query = new Parse.Query(Dashboard);
 
-    const totalCount = urbanRuralData.length;
-    const counts = urbanRuralData.reduce((acc, value) => {
-      acc[value] = (acc[value] || 0) + 1;
-      return acc;
-    }, {});
+      // Filtros específicos
+      query.equalTo('SG_UF', 'PE');
+      query.equalTo('NO_MUNICIPIO', 'Paulista');
 
-    const percentages = {};
-    for (const [key, value] of Object.entries(counts)) {
-      percentages[key] = ((value / totalCount) * 100).toFixed(2);
-    }
+      try {
+        const results = await query.find();
+        const urbanRuralData = results.map(school => school.get('TP_LOCALIZACAO'));
 
-    const seriesData = Object.entries(percentages).map(([key, value]) => {
-      let area;
-      if (key === '1') {
-        area = 'Área Urbana';
-      } else {
-        area = 'Área Rural';
+        const totalCount = urbanRuralData.length;
+        const counts = urbanRuralData.reduce((acc, value) => {
+          acc[value] = (acc[value] || 0) + 1;
+          return acc;
+        }, {});
+
+        const columnMapping = {
+          "1": "Área Urbana",
+          "2": "Área Rural"
+        };
+
+        const seriesData = Object.entries(counts).map(([key, value]) => {
+          return {
+            x: columnMapping[key] || key,
+            y: ((value / totalCount) * 100).toFixed(2),
+          };
+        });
+
+        const chartData = {
+          options: {
+            colors: ["#92FD8CFF"],
+            chart: {
+              type: 'line',
+            },
+            xaxis: {
+              categories: seriesData.map(data => data.x),
+            },
+          },
+          series: [
+            {
+              name: 'Porcentagem',
+              data: seriesData.map(data => data.y),
+            },
+          ],
+        };
+
+        setChartData(chartData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-
-      return {
-        x: area,
-        y: parseFloat(value),
-      };
-    });
-
-    const chartData = {
-      options: {
-        colors: ["#92FD8CFF"],
-        chart: {
-          type: 'line',
-        },
-        xaxis: {
-          categories: seriesData.map(data => data.x),
-        },
-      },
-      series: [
-        {
-          name: 'Porcentagem',
-          data: seriesData.map(data => data.y),
-        },
-      ],
     };
 
-    setChartData(chartData);
+    fetchUrbanRuralData();
   }, []);
 
   if (Object.keys(chartData).length === 0) {
@@ -71,11 +75,11 @@ const UrbanRuralSchoolsChart = () => {
 
   return (
     <div>
-      const element = <h1 style={{color: 'white'}}>Distribuição escolas áreas urbanas e rurais em Paulista - PE
-    </h1>
+      <h1 style={{ color: 'white' }}>Distribuição escolas áreas urbanas e rurais em Paulista - PE</h1>
       <Chart options={chartData.options} series={chartData.series} type="line" height={400} />
     </div>
   );
 };
 
 export default UrbanRuralSchoolsChart;
+  
--- src/components/TipoPredio.js
+++ src/components/TipoPredio.js
@@ -1,77 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
+import Parse from 'parse/dist/parse.min.js';
 
-const SchoolBuildingChart = () => {
+Parse.initialize(
+  'e5UX3sOvZfOY3qwvIdw0b7v18JdUn9ihz34chyhF', // Application ID
+'rSRY7ybwkJThcFwIQirObv3v83Bfa40LWPyaZQNf' // JavaScript key
+);
+Parse.serverURL = 'https://parseapi.back4app.com';
+
+const SchoolStatusChart = () => {
   const [chartData, setChartData] = useState({});
 
   useEffect(() => {
-    const buildingTypeData = [
-      1, 2, 1, 1, 1, 1, 3, 1, 1, 1,
-      1, 2, 1, 1, 1, 1, 3, 1, 2, 1,
-      1, 1, 3, 1, 1, 1, 1, 2, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 3, 3, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    ];
+    const fetchSchoolStatusData = async () => {
+      const Dashboard = Parse.Object.extend('Dashboard');
+      const query = new Parse.Query(Dashboard);
 
-    const totalCount = buildingTypeData.length;
-    const counts = buildingTypeData.reduce((acc, value) => {
-      acc[value] = (acc[value] || 0) + 1;
-      return acc;
-    }, {});
+      // Filtros específicos
+      query.equalTo('SG_UF', 'BA');
+      query.equalTo('NO_MUNICIPIO', 'CamaÃ§ari');
 
-    const percentages = {};
-    for (const [key, value] of Object.entries(counts)) {
-      percentages[key] = ((value / totalCount) * 100).toFixed(2);
-    }
+      try {
+        const results = await query.find();
+        const schoolStatusData = results.map(school => school.get('TP_OCUPACAO_PREDIO_ESCOLAR'));
 
-    const seriesData = Object.entries(percentages).map(([key, value]) => {
-      let type;
-      switch (key) {
-        case '1':
-          type = 'Próprio';
-          break;
-        case '2':
-          type = 'Alugado';
-          break;
-        case '3':
-          type = 'Cedido';
-          break;
-        default:
-          type = 'Não aplicável';
-          break;
+        const totalCount = schoolStatusData.length;
+        const counts = schoolStatusData.reduce((acc, value) => {
+          acc[value] = (acc[value] || 0) + 1;
+          return acc;
+        }, {});
+
+        const columnMapping = {
+          "1": "Próprio",
+          "2": "Alugado",
+          "3": "Cedido",
+          "undefined" : "não é prédio escolar",
+        };
+
+        const seriesData = Object.entries(counts).map(([key, value]) => {
+          return {
+            x: columnMapping[key] || key,
+            y: value,
+          };
+        });
+
+        const chartData = {
+          options: {
+            colors: ["#92fd8c", "#ff5959", "#a6a6a6"],
+            chart: {
+              type: 'bar',
+            },
+            xaxis: {
+              categories: seriesData.map(data => data.x)
+            },
+          },
+          series: [{
+            name: 'Número de Escolas',
+            data: seriesData.map(data => data.y)
+          }],
+        };
+
+        setChartData(chartData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-
-      return {
-        x: type,
-        y: parseFloat(value),
-      };
-    });
-
-    const chartData = {
-      options: {
-        colors:["#92FD8CFF"],
-        chart: {
-          type: 'area',
-        },
-        xaxis: {
-          type: 'category',
-          categories: seriesData.map(data => data.x),
-        },
-      },
-      series: [
-        {
-          name: 'Porcentagem',
-          data: seriesData.map(data => data.y),
-        },
-      ],
     };
 
-    setChartData(chartData);
+    fetchSchoolStatusData();
   }, []);
 
   if (Object.keys(chartData).length === 0) {
@@ -80,11 +75,10 @@ const SchoolBuildingChart = () => {
 
   return (
     <div>
-      const element = <h1 style={{ color: 'white' }}>Tipo de ocupação em prédio escolar em Camaçari - BA
-    </h1>
+      <h1 style={{ color: 'white' }}>Ocupação do Prédio Escolar em Camaçari - BA</h1>
       <Chart options={chartData.options} series={chartData.series} type="area" height={400} />
     </div>
   );
 };
 
-export default SchoolBuildingChart;
+export default SchoolStatusChart;
